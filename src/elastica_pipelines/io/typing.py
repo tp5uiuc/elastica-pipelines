992236485d5f7459e28450980a8bd36b91e8c0c7
tp5uiuc <tp5@illinois.edu>
2022-08-28 21:53:16 -0500

    fix : typing updates

--- src/elastica_pipelines/io/typing.py
+++ src/elastica_pipelines/io/typing.py
@@ -1,6 +1,8 @@
 """Elastica IO typing."""
 from typing import Any
+from typing import Generic
 from typing import Mapping
+from typing import TypeVar
 from typing import Union
 
 import numpy.typing as npt
@@ -10,6 +12,20 @@ from typing_extensions import TypeAlias
 Key: TypeAlias = Union[int, str]
 Node: TypeAlias = Mapping[str, Any]
 Index: TypeAlias = Union[int, slice]
-Record: TypeAlias = Mapping[Key, npt.ArrayLike]
-Records: TypeAlias = Mapping[Key, Record]
-RecordsSlice: TypeAlias = Mapping[Key, Record]
+Record = TypeVar("Record", bound=Mapping[Key, npt.ArrayLike], covariant=True)
+
+
+class _R(Generic[Record]):
+    pass
+
+
+ConcreteRecord = TypeVar("ConcreteRecord", bound=_R[Mapping[Key, npt.ArrayLike]])
+Records: TypeAlias = Mapping[Key, ConcreteRecord]
+RecordsSlice: TypeAlias = Mapping[Key, ConcreteRecord]
+
+
+"""Old way of defining types, mypy deficiencies are rampant smh.
+# Record: TypeAlias = Mapping[Key, npt.ArrayLike]
+# Records = TypeVar("Records", bound=Mapping[Key, ConcreteRecord])
+# RecordsSlice = TypeVar("RecordsSlice", bound=Mapping[Key, ConcreteRecord])
+"""
