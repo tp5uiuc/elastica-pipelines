27c33e2a088d119c65c1c2ade2926a44af448266
Tejaswin Parthasarathy <56932502+tp5uiuc@users.noreply.github.com>
2022-09-01 05:49:32 -0500

    feat : core io series (#34)

--- tests/io/test_temporal.py
+++ tests/io/test_temporal.py
@@ -1,4 +1,9 @@
 """Test cases for temporal IO types."""
+from dataclasses import dataclass
+from typing import Any
+from typing import Dict
+from typing import Tuple
+
 import pytest
 
 from elastica_pipelines.io.core import SystemRecord
@@ -9,6 +14,8 @@ from elastica_pipelines.io.specialize import SphereRecords
 from elastica_pipelines.io.specialize import SphereRecordTraits
 from elastica_pipelines.io.temporal import RecordsAdapter
 from elastica_pipelines.io.temporal import RecordsAdapterKey
+from elastica_pipelines.io.temporal import Series
+from elastica_pipelines.io.temporal import SeriesKey
 from elastica_pipelines.io.temporal import Snapshot
 from elastica_pipelines.io.typing import Node
 from tests.io.test_core import node_v  # noqa : F401
@@ -195,3 +202,116 @@ class TestSnapshot:
 
         assert len(sl) == 4  # four systems in total
         assert list(map(lambda x: x.sys_id, sl.keys())) == [0, 1, 0, 1]
+
+
+@dataclass
+class WrappedFloat:
+    """Wraps a scalar float with a HDF5 style getitem access."""
+
+    value: float
+
+    def __getitem__(self, key: Tuple[Any, ...]) -> float:  # noqa
+        return self.value
+
+
+def temporal_information(it: int) -> Dict[str, Dict[str, WrappedFloat]]:
+    """Construct temporal information."""
+    return {"TimeMetadata": {"time": WrappedFloat(it * 0.1), "dt": WrappedFloat(0.02)}}
+
+
+@pytest.fixture
+def series_node(snap_node) -> Node:
+    """Gets node data for a snapshot.
+
+    Returns:
+       node with data.
+    """
+
+    def prepare_node(it: int) -> Node:
+        return {
+            ElasticaConvention.as_record_key(it): dict(
+                **{"data": snap_node}, **temporal_information(it)
+            )
+        }
+
+    return dict(
+        **prepare_node(50), **dict(prepare_node(100), **dict(prepare_node(150)))
+    )
+
+
+class TestSeries:
+    """Test series-related functionality."""
+
+    def test_getitem(self, series_node) -> None:
+        """Test getitem.
+
+        Args:
+            series_node : The fixture to obtain series node data.
+        """
+        s = Series(series_node)
+
+        assert len(s) == 3
+
+        sl = s[50]
+        assert isinstance(sl, Snapshot)
+        assert sl == Snapshot(series_node["000050"]["data"])
+
+        sl = s[100]
+        assert isinstance(sl, Snapshot)
+        assert sl == Snapshot(series_node["000100"]["data"])
+
+        # Getitem test with SeriesKey
+        k = SeriesKey(100, 10.0, 0.02)
+        sl = s[k]
+        assert isinstance(sl, Snapshot)
+        assert sl == Snapshot(series_node["000100"]["data"])
+
+    def test_transforms(self, series_node) -> None:
+        """Test transformation.
+
+        Args:
+            series_node : The fixture to obtain series node data.
+        """
+
+        def trafo(x):
+            return x + 2
+
+        s = Series(series_node, transforms=trafo)
+
+        sl = s[50]
+        assert isinstance(sl, Snapshot)
+        assert sl == Snapshot(series_node["000050"]["data"], transforms=trafo)
+
+    # FIXME : Typeguard fails with a weird NameError not related to the test.
+    @skip_if_env_has("typeguard")
+    def test_iter(self, series_node) -> None:
+        """Test iterator.
+
+        Args:
+            series_node : The fixture to obtain series node data.
+        """
+        s = Series(series_node)
+
+        # There are two keys
+        its = iter(s)
+        assert next(its) == SeriesKey(50, 5.0, 0.02)
+        assert next(its) == SeriesKey(100, 10.0, 0.02)
+        assert next(its) == SeriesKey(150, 15.0, 0.02)
+        assert iter(its) == its
+
+    # FIXME : Typeguard fails with a weird NameError not related to the test.
+    @skip_if_env_has("typeguard")
+    def test_iterations(self, series_node) -> None:
+        """Test iterations/items.
+
+        Args:
+            series_node : The fixture to obtain series node data.
+        """
+        s = Series(series_node)
+
+        for t, snaps in s.iterations():
+            assert t.dt == 0.02
+            TestSnapshot().test_getitem(snaps.node)
+            TestSnapshot().test_iter(snaps.node)
+            TestSnapshot().test_mixins(snaps.node)
+            # TestSnapshot().test_systems(snaps.node)
