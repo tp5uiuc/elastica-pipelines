fd7ffeb20d9da64fc5c468e2f0f7593ffe931832
tp5uiuc <tp5@illinois.edu>
2022-08-31 23:37:23 -0500

    feat : snapshot data-structure

--- src/elastica_pipelines/io/temporal.py
+++ src/elastica_pipelines/io/temporal.py
@@ -1,11 +1,26 @@
 """Temporal IO types."""
 from __future__ import annotations
 
+from collections.abc import Iterator
 from dataclasses import dataclass
+from typing import Any
+from typing import ChainMap
+from typing import Dict
 from typing import Mapping
+from typing import Optional
+from typing import Protocol
+from typing import Type
+
+from typing_extensions import TypeAlias
 
 from elastica_pipelines.io.core import SystemRecords
 from elastica_pipelines.io.protocols import name
+from elastica_pipelines.io.specialize import CosseratRodRecords
+from elastica_pipelines.io.specialize import CosseratRodRecordTraits
+from elastica_pipelines.io.specialize import SphereRecords
+from elastica_pipelines.io.specialize import SphereRecordTraits
+from elastica_pipelines.io.typing import FuncType
+from elastica_pipelines.io.typing import Node
 from elastica_pipelines.io.typing import RecordLeafs
 
 
@@ -61,3 +76,77 @@ class RecordsAdapter(Mapping[RecordsAdapterKey, RecordLeafs]):
         if k.sys_type is not name(self.records):
             raise KeyError(f"{k.sys_type}")
         return self.records[k.sys_id]
+
+
+class HasGetItem(Protocol):
+    """Get item protocol."""
+
+    def __getitem__(self, k: str) -> Any:  # noqa
+        ...  # pragma: no cover
+
+
+class CosseratRodRecordsMixin:
+    """Mixin for cosserat_rods() access."""
+
+    def cosserat_rods(self: HasGetItem) -> CosseratRodRecords:
+        """Access cosserat rod records.
+
+        Returns:
+            CosseratRod records.
+        """
+        return self.__getitem__(CosseratRodRecordTraits.name())
+
+
+class SphereRecordsMixin:
+    """Mixin for spheres() access."""
+
+    def spheres(self: HasGetItem) -> SphereRecords:
+        """Access sphere records.
+
+        Returns:
+            Sphere records.
+        """
+        return self.__getitem__(SphereRecordTraits.name())
+
+
+RecordsMap: TypeAlias = Mapping[str, SystemRecords]
+
+
+class Snapshot(RecordsMap, CosseratRodRecordsMixin, SphereRecordsMixin):
+    """Data access for a single snapshot of an Elastica++ simulation.
+
+    Args:
+        node (node): Node in which to lookup the temporal information.
+        transforms (callable, optional): A function/transform that takes in an array
+            data-structure and returns a transformed version.
+            E.g, ``transforms.ToArray``
+    """
+
+    def __init__(self, node: Node, transforms: Optional[FuncType] = None) -> None:
+        """Initializer."""
+        self.node = node
+        self.transforms = transforms
+        # mypy complains about HasRecordTraits not being met.
+        self.return_lut: Dict[str, Type[SystemRecords]]
+        self.return_lut = {
+            name(x): x for x in SystemRecords.__subclasses__()  # type: ignore[arg-type]
+        }
+
+    def __getitem__(self, k: str) -> SystemRecords:  # noqa
+        return_type = self.return_lut[k]
+        return return_type(self.node[k], self.transforms)
+
+    def __iter__(self) -> Iterator[str]:  # noqa
+        return iter(self.node)
+
+    def __len__(self) -> int:  # noqa
+        return len(self.node)
+
+    def systems(self) -> ChainMap[RecordsAdapterKey, RecordLeafs]:
+        """Access all system records.
+
+        Returns:
+            Records across all systems.
+        """
+        # map() does not play well with inference.
+        return ChainMap(*map(RecordsAdapter, self.values()))  # type: ignore[arg-type]
--- tests/io/test_temporal.py
+++ tests/io/test_temporal.py
@@ -2,8 +2,15 @@
 import pytest
 
 from elastica_pipelines.io.core import SystemRecord
+from elastica_pipelines.io.protocols import ElasticaConvention
+from elastica_pipelines.io.specialize import CosseratRodRecords
+from elastica_pipelines.io.specialize import CosseratRodRecordTraits
+from elastica_pipelines.io.specialize import SphereRecords
+from elastica_pipelines.io.specialize import SphereRecordTraits
 from elastica_pipelines.io.temporal import RecordsAdapter
 from elastica_pipelines.io.temporal import RecordsAdapterKey
+from elastica_pipelines.io.temporal import Snapshot
+from elastica_pipelines.io.typing import Node
 from tests.io.test_core import node_v  # noqa : F401
 from tests.io.test_core import records_v  # noqa : F401
 from tests.io.test_protocols import skip_if_env_has
@@ -43,9 +50,8 @@ class TestRecordsAdapter:
             assert k.sys_id == idx
             assert k.sys_type == "System"
 
-    @skip_if_env_has(
-        "typeguard"
-    )  # Typeguard fails with a weird NameError not related to the test.
+    # FIXME : Typeguard fails with a weird NameError not related to the test.
+    @skip_if_env_has("typeguard")
     def test_getitem(self, records_v) -> None:  # noqa : F811
         """Getitem test.
 
@@ -66,3 +72,126 @@ class TestRecordsAdapter:
             RecordsAdapterKey("System", 4),
         )  # id is incorrect
         assert all(map(test_key_error, incorrect_keys))
+
+
+@pytest.fixture
+def snap_node() -> Node:
+    """Gets node data for a snapshot.
+
+    Returns:
+       node with data.
+    """
+    # TODO : revisit this later on.
+    def wrap(x):
+        return {"data": x}
+
+    cosserat_rod_records = {
+        ElasticaConvention.as_system_key(0): {
+            "Position": wrap(2.0),
+            "Velocity": wrap(3.0),
+            "Curvature": wrap(4.0),
+        },
+        ElasticaConvention.as_system_key(1): {
+            "Position": wrap(4.0),
+            "Velocity": wrap(6.0),
+            "Curvature": wrap(8.0),
+        },
+    }
+    sphere_records = {
+        ElasticaConvention.as_system_key(0): {
+            "Position": wrap(1.0),
+            "Velocity": wrap(2.0),
+        },
+        ElasticaConvention.as_system_key(1): {
+            "Position": wrap(5.0),
+            "Velocity": wrap(8.0),
+        },
+    }
+    return {
+        CosseratRodRecordTraits.name(): cosserat_rod_records,
+        SphereRecordTraits.name(): sphere_records,
+    }
+
+
+class TestSnapshot:
+    """Test snapshot-related functionality."""
+
+    def test_getitem(self, snap_node) -> None:
+        """Test getitem.
+
+        Args:
+            snap_node : The fixture to obtain node data.
+        """
+        s = Snapshot(snap_node)
+
+        assert len(s) == 2
+
+        sl = s["CosseratRod"]
+        assert isinstance(sl, CosseratRodRecords)
+        assert sl == CosseratRodRecords(snap_node["CosseratRod"])
+
+        sl = s["Sphere"]
+        assert isinstance(sl, SphereRecords)
+        assert sl == SphereRecords(snap_node["Sphere"])
+
+    def test_transforms(self, snap_node) -> None:
+        """Test transformation.
+
+        Args:
+            snap_node : The fixture to obtain node data.
+        """
+
+        def trafo(x):
+            return x + 2
+
+        s = Snapshot(snap_node, transforms=trafo)
+        sl = s["CosseratRod"]
+        assert isinstance(sl, CosseratRodRecords)
+        assert sl == CosseratRodRecords(snap_node["CosseratRod"], transforms=trafo)
+
+    def test_iter(self, snap_node) -> None:
+        """Test iterator.
+
+        Args:
+            snap_node : The fixture to obtain node data.
+        """
+        s = Snapshot(snap_node)
+
+        # There are two keys
+        its = iter(s)
+        assert next(its) == "CosseratRod"
+        assert next(its) == "Sphere"
+
+        # Test iterator directly via yield
+        assert s.keys() == snap_node.keys()
+
+    def test_mixins(self, snap_node) -> None:
+        """Test mixins.
+
+        Args:
+            snap_node : The fixture to obtain node data.
+        """
+        s = Snapshot(snap_node)
+
+        sl = s.cosserat_rods()
+        assert isinstance(sl, CosseratRodRecords)
+        assert sl == CosseratRodRecords(snap_node["CosseratRod"])
+
+        sl = s.spheres()
+        assert isinstance(sl, SphereRecords)
+        assert sl == SphereRecords(snap_node["Sphere"])
+
+    # FIXME : Typeguard fails with a weird NameError not related to the test.
+    @skip_if_env_has("typeguard")
+    def test_systems(self, snap_node) -> None:
+        """Test systems() call.
+
+        Args:
+            snap_node : The fixture to obtain node data.
+        """
+        s = Snapshot(snap_node)
+
+        sl = s.systems()
+
+        assert len(sl) == 4  # four systems in total
+        assert list(map(lambda x: x.sys_id, sl.keys())) == [0, 1, 0, 1]
