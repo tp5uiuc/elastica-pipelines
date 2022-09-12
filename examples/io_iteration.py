6b2ce1ffb51d85085411b942a895ac5c0a7647e1
tp5uiuc <tp5@illinois.edu>
2022-09-12 08:25:17 -0500

    feat : io iteration example

---
+++ examples/io_iteration.py
@@ -0,0 +1,28 @@
+"""Lazy loading of Elastica++ simulation data."""
+from pathlib import Path
+
+from elastica_pipelines import io
+
+
+# Metadata file written by Elastica++.
+metadata_fn = Path("..") / "tests" / "io" / "data" / "elastica_metadata.h5"
+
+# Create read only access to data written by Elastica++.
+series = io.series(metadata=metadata_fn)
+
+# use series like a python Mapping.
+for t, snapshot in series.iterations():
+    print(f"Iteration: {t.iterate} at time {t.time}")
+
+    # Snapshot is a mapping contain system types such as CosseratRods & Spheres.
+    # Here we access only cosserat rods only.
+    for (
+        rod_id,
+        rod,
+    ) in snapshot.cosserat_rods().items():  # snapshot['CosseratRod'] also works!
+        if rod_id == 0:
+            print(f"  Rod '{rod_id}' attributes:")
+            # Even rod is a Mapping, so we get its keys...
+            print(f"  {list(rod.keys())}")
+        # and then access it values.
+        print(f"  Rod '{rod_id}' position:", rod["Position"])
